d1e425b78de07e31def2ead60809038bf5f1e317
naavika <niksshahiruchi@gmail.com>
2024-08-01 16:28:20 +0530

    project-one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import ExpenseItem  from './components/ExpenseItem';
 import React from 'react';
 
 function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
-      <p>I am learning React</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
