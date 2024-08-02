864e0c0e5a09e5b9828c7b4b2fd83a304915a9b8
naavika <niksshahiruchi@gmail.com>
2024-08-02 20:06:19 +0530

    project-one

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from 'react';
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () =>{
   const expenses = [
     { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
     { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
