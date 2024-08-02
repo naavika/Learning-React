ba6c100e556e5f813184e4dbf0870f2383161b36
naavika <niksshahiruchi@gmail.com>
2024-08-02 15:47:18 +0530

    project-one

---
+++ src/components/Expenses.js
@@ -0,0 +1,19 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          date={expense.date}
+          title={expense.title}
+          price={expense.price}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;
