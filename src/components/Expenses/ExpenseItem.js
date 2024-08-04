7d08d9630bdf3cc1f39b674ce9ad41fb7db9d0e1
naavika <niksshahiruchi@gmail.com>
2024-08-04 15:51:22 +0530

    project-one

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  function buttonClickHandler(event) {
+    console.log("Button clicked");
+  }
    return (
     <Card className ="expense-item">  
       <ExpenseDate date ={props.date}></ExpenseDate>
@@ -11,6 +14,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
+      <button onClick={buttonClickHandler}>Change Title</button>
     </Card>
   );
 }
